4986c8f2645da23b2f2e9e392863e350f04b60d3
Stefan Braicu <sbraicu@cisco.com>
2022-09-25 04:18:39 -0700

    caddy

--- content/lab/init/initial_setup.sh
+++ content/lab/init/initial_setup.sh
@@ -4,8 +4,15 @@
 sudo swapoff -a
 sudo sed -i '/ swap / s/^/#/' /etc/fstab
 
-#install docker
+
+#caddy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
+sudo apt install caddy
+
+#install docker
 sudo apt -y install docker.io
 
 #install kubectl
