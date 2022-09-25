f8bdd1c3d1e2d1f0c8b2eacd35748c3918418954
Stefan Braicu <sbraicu@cisco.com>
2022-09-25 02:23:27 -0700

    initial setup script

---
+++ content/lab/init/initial_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#disable cache
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+#install docker
+sudo apt update
+sudo apt install docker.io
+
+##install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.15.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/bin/kind
