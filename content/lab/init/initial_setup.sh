6375c803365120215e7974d4c6bf37fbb863511e
Stefan Braicu <sbraicu@cisco.com>
2022-09-25 02:36:54 -0700

    initial setup

--- content/lab/init/initial_setup.sh
+++ content/lab/init/initial_setup.sh
@@ -6,9 +6,14 @@ sudo sed -i '/ swap / s/^/#/' /etc/fstab
 
 #install docker
 sudo apt update
-sudo apt install docker.io
+sudo apt -y install docker.io
 
 ##install kind
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.15.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/bin/kind
+
+
+#allow current user to run docker
+sudo usermod -aG docker $USER
+sudo service ttyd restart
